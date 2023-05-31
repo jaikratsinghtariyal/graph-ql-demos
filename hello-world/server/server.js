a7f2e79f9db66114fa0fde93268a9bae3c7ab8fd
Jaikrat Singh Tariyal <tektutorialfeedback@gmail.com>
2023-05-31 23:22:34 +0100

    first commit

---
+++ hello-world/server/server.js
@@ -0,0 +1,23 @@
+import {ApolloServer} from '@apollo/server'
+import {startStandaloneServer } from '@apollo/server/standalone'
+
+
+const typeDefs = `#graphql
+    type Query {
+        greeting: String
+    }
+`;
+
+const resolvers = {
+    Query :{
+        greeting: () => "Hello World", 
+    },
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+//const info = await startStandaloneServer(server, {listen : {port : 9000}});
+const {url} = await startStandaloneServer(server, {listen : {port : 9000}});
+
+
+//console.log(`server running at ${info.url}`);
+console.log(`server running at ${url}`);
